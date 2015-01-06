55be1edfb832c04eff6d2512190642bb0e066173
Brent Heftye <brent@locker.com.mx>
2015-01-06 10:20:02 -0600

    Cambios Header y Ejemplos #

--- includes/header.php
+++ includes/header.php
@@ -16,9 +16,9 @@ include_once('lang/'.$idioma.'.php');
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<?php include_once("includes/path.php"); ?>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<meta name="description" content="Locker Agencia Creativa" />
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta name="description" content="Locker Agencia Creativa" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         <meta name="keywords" content="Locker, Diseño Web, Programacion Web, Redes Sociales, Publicidad, Imagen, Responsive, Webs, PHP, HTML5, CSS3, " /> <!-- IMPORTANTE -->
         <meta name="author" content="Locker AD" /><!-- Aqui siempre va Locker AD -->
         <meta property="og:title" content="Locker - Don Porfirio" /> <!-- En el caso de un portafolio, se pone el titulo del Portafolio, -->
@@ -28,11 +28,13 @@ include_once('lang/'.$idioma.'.php');
         <meta property="og:image" content="http://clientes.locker.com.mx/donporfirio/img/logo.png" /><!-- Path de la Imagen Principal del Proyecto -->
         <meta property="og:locale" content="en_US" />
         <meta property="og:site_name" content="Locker Agencia Creativa" /> <!-- Nombre de la Empresa -->
+
         <link rel='shortcut icon' href='favicon.ico'> 
         <link href="<?=mypath?>css/bootstrap.css" rel="stylesheet">
-		<link href="<?=mypath?>css/style.css" rel="stylesheet">
-		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-<title>Don Porfirio</title>
+        <link href="<?=mypath?>css/style.css" rel="stylesheet">
+        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+        <title>Don Porfirio</title>
 </head>
 <body>
 <div id="wraper">
--- index.php
+++ index.php
@@ -1,2 +1,16 @@
 <?php include_once('includes/header.php') ?>
+<!-- Website Content -->
+	<a href="#news">News</a>
+	<a href="#work">Work</a>
+	<a href="#about">About</a>
+	<a href="#contact">Contact</a>
+	<div>
+		<div>
+			<a href="#winter_olympics_2013" onclick="verProyecto(2)">Winter Olympics
+				<video></video>
+			</a>
+		</div>
+	</div>
+
+<!-- Website Content -->
 <?php include_once('includes/footer.php') ?>
