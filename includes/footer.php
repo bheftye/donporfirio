cbe60e42d762df4d4bfaeece1e0d77d6a8ec18f0
Shieldbro <israel@locker.com.mx>
2015-01-06 10:04:15 -0600

    Primeros archivos de inicio

---
+++ includes/footer.php
@@ -0,0 +1,18 @@
+</div>
+</div>
+<script src="http://code.jquery.com/jquery-2.1.3.min.js"></script>
+<script src="http://code.jquery.com/ui/1.11.2/jquery-ui.min.js"></script>
+<script src="<?=mypath?>js/bootstrap.min.js"></script>
+</body>
+</html>
+<script>
+/*
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-24233015-24', 'locker.com.mx');
+  ga('send', 'pageview');
+*/
+</script>
---
+++ includes/header.php
@@ -0,0 +1,39 @@
+<?php
+if(isset($_REQUEST['idioma'])){
+	if($_REQUEST['idioma']=='en'){
+		$idioma = 'en';
+	}
+	else{
+		$idioma = 'es';
+	}
+}
+else{
+	$idioma = 'es';
+}
+include_once('lang/'.$idioma.'.php');
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<?php include_once("includes/path.php"); ?>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="description" content="Locker Agencia Creativa" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <meta name="keywords" content="Locker, Diseño Web, Programacion Web, Redes Sociales, Publicidad, Imagen, Responsive, Webs, PHP, HTML5, CSS3, " /> <!-- IMPORTANTE -->
+        <meta name="author" content="Locker AD" /><!-- Aqui siempre va Locker AD -->
+        <meta property="og:title" content="Locker - Don Porfirio" /> <!-- En el caso de un portafolio, se pone el titulo del Portafolio, -->
+        <meta property="og:url" content="http://clientes.locker.com.mx/donporfirio" /> <!-- Este es Link que Facebook Tomara, por eso le pasamos el ID, si es un index o nosotros, solo va la pagina EJ: locker.com.mx -->
+        <meta property="og:type" content="website" />
+        <meta property="og:description" content="Descripcion" /><!-- Descripción de Algún portafolio o proyecto, si es Index o Nosotros, poner Breve Descripción. -->
+        <meta property="og:image" content="http://clientes.locker.com.mx/donporfirio/img/logo.png" /><!-- Path de la Imagen Principal del Proyecto -->
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:site_name" content="Locker Agencia Creativa" /> <!-- Nombre de la Empresa -->
+        <link rel='shortcut icon' href='favicon.ico'> 
+        <link href="<?=mypath?>css/bootstrap.css" rel="stylesheet">
+		<link href="<?=mypath?>css/style.css" rel="stylesheet">
+		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+<title>Don Porfirio</title>
+</head>
+<body>
+<div id="wraper">
+<div id="wraperfondo">
