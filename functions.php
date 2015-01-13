53f9f9393117743ef8634ab4b99f37247722a020
Brent Heftye <brent@locker.com.mx>
2015-01-13 09:21:41 -0600

    Proyecto Siguiente | Proyecto anterior

--- functions.php
+++ functions.php
@@ -18,6 +18,19 @@ switch ($operaciones) {
 		$id_categoria = $_REQUEST["id_categoria"];
 		$proyecto = new proyecto();
 		$proyecto -> listar_proyecto_categoria_ajax($id_categoria);
+	case "obtener_proyecto_siguiente":
+		$id_proyecto = $_REQUEST["id_proyecto"];
+		$proyecto = new proyecto($id_proyecto);
+		$id_proyecto_siguiente = $proyecto -> obtener_id_proyecto_siguiente();
+		$proyecto = new proyecto($id_proyecto_siguiente);
+		$proyecto -> obtener_proyecto_ajax();
+	break;
+	case "obtener_proyect_anterior":
+		$id_proyecto = $_REQUEST["id_proyecto"];
+		$proyecto = new proyecto($id_proyecto);
+		$id_proyecto_anterior = $proyecto -> obtener_id_proyecto_anterior();
+		$proyecto = new proyecto($id_proyecto_anterior);
+		$proyecto -> obtener_proyecto_ajax();
 	break;
 	default:
 	break;
