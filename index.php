1824596d842bd2061e0a7e5b45544e75b4e7850f
Shieldbro <israel@locker.com.mx>
2015-01-14 13:52:50 -0600

    mas cambios

--- index.php
+++ index.php
@@ -151,10 +151,14 @@
 			$subtitulo_proyecto = ($idioma == "es")? $un_proyecto["subtitulo_esp"] : $un_proyecto["subtitulo_eng"];
 			$video_preview = $un_proyecto["nombre_preview"];
 			$vp = "'".$video_preview."'";
-			echo '<a href="#'.$un_proyecto["url_amigable"].'" style="display:block;" onclick="verproyecto('.$un_proyecto["id_proyecto"].')" onmouseenter="showpreview('.$un_proyecto["id_proyecto"].','.$vp.')" onmouseleave="hidepreview('.$un_proyecto["id_proyecto"].')"><div style="max-height:200px; overflow:hidden;"  class="proyectofondo" >
+			echo '<a href="#'.$un_proyecto["url_amigable"].'" style="display:block;" onclick="verproyecto('.$un_proyecto["id_proyecto"].')" onmouseenter="showpreview('.$un_proyecto["id_proyecto"].','.$vp.')" onmouseleave="hidepreview('.$un_proyecto["id_proyecto"].')">
+					<div style="max-height:200px; overflow:hidden; position:relative;"  class="proyectofondo" >
 					<img style="width:100%; margin: 1px 0;" src="'.mypath.'imgProyectos/'.$un_proyecto["img_principal"].'" />
+					<div class="fcategoria"></div>
+					<h3>'.$titulo_proyecto.'</h3>
 					<div class="vidpreview videono'.$un_proyecto["id_proyecto"].'"></div>
-				</div></a>';
+					</div>
+				</a>';
 		}
 		?>
 		</div>
--- js/scripts.js
+++ js/scripts.js
@@ -44,12 +44,12 @@ function closegallery(){
 }
 
 function viewcontact(){
-	//$("#wraperfondo").animate({"top":"-400px"},600);
-	//$(".contenido").animate({"top":"-400px"},600);
+	$("#wraperfondo").animate({"top":"-400px"},600);
+	$(".contenido").animate({"top":"-400px"},600);
 	$("#contacto").animate({"top":"-400px"},600);
-	//$("#bgvid").animate({"top":"-400px"},600);
-	//$("#bgvid3").animate({"top":"-400px"},600);
-	//$(".aboutbg").animate({"top":"-400px"},600);
+	$("#bgvid").animate({"top":"-400px"},600);
+	$("#bgvid3").animate({"top":"-400px"},600);
+	$(".aboutbg").animate({"top":"-400px"},600);
 }
 
 //$('#contacto').click(function(event){
@@ -58,12 +58,12 @@ function viewcontact(){
 //});
 
 function hidecontact(){
-	//$("#wraperfondo").animate({"top":"0"},600);
-	//$(".contenido").animate({"top":"0"},600);
+	$("#wraperfondo").animate({"top":"0"},600);
+	$(".contenido").animate({"top":"0"},600);
 	$("#contacto").animate({"top":"0"},600);
-	//$("#bgvid").animate({"top":"0"},600);
-	//$("#bgvid3").animate({"top":"0"},600);
-	//$(".aboutbg").animate({"top":"0"},600);
+	$("#bgvid").animate({"top":"0"},600);
+	$("#bgvid3").animate({"top":"0"},600);
+	$(".aboutbg").animate({"top":"0"},600);
 }/*
 function changecursor(){
 	console.log("cursor cambiado");
